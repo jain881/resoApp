77cb617b5068d3b358db7378e7bd7838c8c9c072
jain881 <amanj0314@gmail.com>
2024-10-28 21:26:27 +0530

    Update App.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Sidebar from './components/sidebar'
 import '@coreui/coreui/dist/css/coreui.min.css'
+import RestaurantForm from './components/RestaurantForm'
 
 function App() {
 
   return (
     <>
       <Sidebar />
+      <RestaurantForm />
     </>
   )
 }
